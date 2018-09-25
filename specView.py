e43746636936cc7bc29ada095641e11e65e34c17
schwana <32838066+schwana@users.noreply.github.com>
2018-09-25 15:07:44 +0100

    Draft version
    Charts appear to be working. Data file reads in fine, and individual channels can be viewed in isolation or in combination with other channels.

--- specView.py
+++ specView.py
@@ -21,7 +21,7 @@ matplotlib.use("TkAgg")
 ##from matplotlib.backends.backend_tkagg import FigureCanvasTk, NavigationToolbar2Tk
 ##from matplotlib.figure import Figure
 
-
+N=0
 root = tk.Tk()
 fig = plt.Figure()
 canvas = FigureCanvasTkAgg(fig, root)
@@ -30,7 +30,9 @@ ax = fig.add_subplot(111)
 toolbar = NavigationToolbar2Tk(canvas, root)
 toolbar.update()
 
-class Window(tk.Frame):
+
+        
+class GraphFrame(tk.Frame):
 
     def __init__(self, master=None):
         
@@ -41,9 +43,8 @@ class Window(tk.Frame):
     def init_window(self):
 
         self.master.title("SpecView")
-
         # allowing the widget to take the full space of the root window
-        self.pack(fill=tk.BOTH, expand=1)
+        self.pack(fill=tk.NONE)
 
         # creating a menu instance
         menu = tk.Menu(self.master)
@@ -57,8 +58,9 @@ class Window(tk.Frame):
         edit = tk.Menu(menu)
         edit.add_command(label="Undo")
         menu.add_cascade(label="Edit", menu=edit)
-
         
+
+
     
     def exit(self):
         exit()
@@ -126,6 +128,7 @@ class Window(tk.Frame):
         global H2
         global H3
         global H4
+        global N
 
         iE=iE_
         L5=L5_
@@ -138,35 +141,170 @@ class Window(tk.Frame):
         H2=H2_
         H3=H3_
         H4=H4_
+
+        N=len(iE)
+        print (N,"N")
         
-        self.plot(iE, L3)
+        p=GraphFrame.plot()
+        
         
 
-    def plot(self, t,s):
-    
-        
-        #t = np.arange(0.0,3.0,0.01)
-        #s = np.sin(np.pi*t)
-        ax.plot(t,s)
-                
-        fig.canvas.draw_idle()
-        root.update()  
 
-        
-  
+
             
+    
+    def plot():
+
+        ax.clear()
+        #Check to see what checkboxes are ticked.
+        if Controls.L5_checked.get():
+            ax.plot(iE,L5,color='green')
         
+        if Controls.L4_checked.get():
+            ax.plot(iE,L4,color='red')
+
+        if Controls.L3_checked.get():
+            ax.plot(iE,L3,color='blue')
+
+        if Controls.L2_checked.get():
+            ax.plot(iE,L2,color='cyan')
+
+        if Controls.L1_checked.get():
+            ax.plot(iE,L1,color='yellow')
+
+        if Controls.Ax_checked.get():
+            ax.plot(iE,Ax,color='magenta')            
+
+        if Controls.H1_checked.get():
+            ax.plot(iE,H1,color='black')
+
+        if Controls.H2_checked.get():
+            ax.plot(iE,H2,color='0.25')              
+
+        if Controls.H3_checked.get():
+            ax.plot(iE,H3,color='0.5')  
+
+        if Controls.H4_checked.get():
+            ax.plot(iE,H4,color='0.75')
+
+            
+        fig.canvas.draw_idle()
+        root.update()
+        return 1
+
+    def UpdatePlot():
+        #Check to see if a scan is loaded. If iE is empty then dont call the plot
+        print ("N", N)
+        if (N>0):
+            p=GraphFrame.plot()
+##        else:
+##            print ("iE off")
+
+  
+class Controls(tk.Frame):
+    
+    def __init__(self, root):
+        tk.Frame.__init__(self, root)
 
 
+    def callback(*args):
+        print ("variable changed!")
+        GraphFrame.UpdatePlot()
+        
+    L5_checked = tk.IntVar()
+    L5_checked.trace("w", callback)
+    L5_checked.set(0)
+    
+    L4_checked = tk.IntVar()
+    L4_checked.trace("w", callback)
+    L4_checked.set(0)
+
+    L3_checked = tk.IntVar()
+    L3_checked.trace("w", callback)
+    L3_checked.set(0)
+
+    L2_checked = tk.IntVar()
+    L2_checked.trace("w", callback)
+    L2_checked.set(0)
+
+    L1_checked = tk.IntVar()
+    L1_checked.trace("w", callback)
+    L1_checked.set(0)
+
+    Ax_checked = tk.IntVar()
+    Ax_checked.trace("w", callback)
+    Ax_checked.set(0)
+    
+    H1_checked = tk.IntVar()
+    H1_checked.trace("w", callback)
+    H1_checked.set(0)
+
+    H2_checked = tk.IntVar()
+    H2_checked.trace("w", callback)
+    H2_checked.set(0)
+
+    H3_checked = tk.IntVar()
+    H3_checked.trace("w", callback)
+    H3_checked.set(0)
+
+    H4_checked = tk.IntVar()
+    H4_checked.trace("w", callback)
+    H4_checked.set(0)
+
+
+
+    c1=tk.Checkbutton(root, text="L5", onvalue=1, offvalue=0, variable=L5_checked)
+    c2=tk.Checkbutton(root, text="L4", onvalue=1, offvalue=0, variable=L4_checked)
+    c3=tk.Checkbutton(root, text="L3", onvalue=1, offvalue=0, variable=L3_checked)
+    c4=tk.Checkbutton(root, text="L2", onvalue=1, offvalue=0, variable=L2_checked)
+    c5=tk.Checkbutton(root, text="L1", onvalue=1, offvalue=0, variable=L1_checked)
+    c6=tk.Checkbutton(root, text="Ax", onvalue=1, offvalue=0, variable=Ax_checked)
+    c7=tk.Checkbutton(root, text="H1", onvalue=1, offvalue=0, variable=H1_checked)
+    c8=tk.Checkbutton(root, text="H2", onvalue=1, offvalue=0, variable=H2_checked)
+    c9=tk.Checkbutton(root, text="H3", onvalue=1, offvalue=0, variable=H3_checked)
+    c10=tk.Checkbutton(root, text="H4", onvalue=1, offvalue=0, variable=H4_checked)
+
+    
+    c1.pack(side="left", fill="x")
+    c2.pack(side="left", fill="x")
+    c3.pack(side="left", fill="x")
+    c4.pack(side="left", fill="x")
+    c5.pack(side="left", fill="x")
+    c6.pack(side="left", fill="x")
+    c7.pack(side="left", fill="x")
+    c8.pack(side="left", fill="x")
+    c9.pack(side="left", fill="x")
+    c10.pack(side="left", fill="x")
+        
+    
+
+
+
+
+
+       
+        
         
 # root window created. Here, that would be the only window, but
 # you can later have windows within windows.
 
 
+
 root.geometry("800x600")
 
 #creation of an instance
-app = Window(root)
+graph = GraphFrame(root)
+controls=Controls(root)
+
+graph.pack(side="left", fill="x")
+controls.pack(side="bottom", fill="x")
+
+
+if controls.L5_checked.get():
+    print ("checked")
+
+
+print (controls.L5_checked.get())     
 
 #mainloop 
 root.mainloop()
